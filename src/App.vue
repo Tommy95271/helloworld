cb6c56a6691f6a37bd2fd2e02396695a99d27deb
謝志斌 Tommy <tommy@miniasp.com>
2021-10-12 10:41:44 +0800

    [+] 新增 Component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <VueDirective />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import VueDirective from './components/VueDirective.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    VueDirective
   }
 }
 </script>
---
+++ src/components/VueDirective.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="">
+    <h1>VueDirective</h1>
+    <!-- <button @click.prevent="toggleInputValue">Click me to toggle inputValue</button> -->
+    <form class="" v-if="isVisible" @submit="handleSubmit">
+      <label for="inputValue">inputValue</label>
+      <input
+        type="text"
+        id="inputValue"
+        v-model="inputValue"
+        @keyup.enter="consoleLog(inputValue)"
+      />
+      <span>{{ inputValue }}</span>
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VueDirective",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      msg1: "test",
+      inputValue: "",
+      isVisible: true,
+    };
+  },
+  methods: {
+    toggleInputValue() {
+      this.isVisible = !this.isVisible;
+    },
+    consoleLog(inputValue) {
+      console.log(inputValue);
+    },
+    handleSubmit() {
+      console.log(this.inputValue);
+    },
+  },
+};
+</script>
